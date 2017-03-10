d713170d2d777ccd35b8568ecce0f6f327f7fa16
rafaelkallis <rk@rafaelkallis.com>
2017-03-10 16:30:44 +0100

    inferno 1.3.1, entryPoints in brunch config, updated html

--- brunch-config.js
+++ brunch-config.js
@@ -1,21 +1,23 @@
-module.exports = {
-      npm: {
-            globals: {
-                  Inferno: 'inferno',
-                  Component: 'inferno-component'
-            }
-      },
-      files: {
-            javascripts: { joinTo: 'app.js' },
-            stylesheets: { joinTo: 'app.css' }
-      },
-      plugins: {
-            babel: {
-                  presets: ['es2015'],
-                  plugins: ['inferno']
-            }
-      },
-      modules: {
-            autoRequire: { 'app.js': ['initialize'] }
-      }
-};
+exports.npm = {
+    globals: {
+        Inferno: 'inferno',
+        Component: 'inferno-component'
+    }
+}
+
+exports.files = {
+    javascripts: {
+        entryPoints: {
+            'app/initialize.js': 'app.js'
+        }
+    },
+    stylesheets: {
+        joinTo: 'app.css'
+    }
+}
+
+exports.modules = {
+    autoRequire: {
+        'app.js': ['initialize']
+    }
+}
